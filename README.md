ae58bcc09c6aff71b15fe357f481a0db39e7820a
Prodip Sarkar <ohm.sust@gmail.com>
2016-04-02 07:54:20 +0600

    First Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# classnotif
-University Project
--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('Privater Route Hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+
+		console.log('Request:' + new Date().toString()+' ' + req.method+' '+ req.originalUrl);
+		next();
+	}
+};
+
+// Must Call before any route hit
+// app.use(middleware.requireAuthentication);
 
 
-app.get('/', function (req, res) {
-	res.send('Hello World');
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+	res.send('About Us');
 });
 
 
+app.use(middleware.logger);
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
+
+app.listen(PORT, function (){
+	console.log('Server started at port '+ PORT +' ... ... ...')
+});
